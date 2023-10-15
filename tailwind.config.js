c568f2df91e6046e439da228f7df42ae41eeb12b
Neluttu <neluttu@gmail.com>
2023-10-16 00:31:03 +0300

    Done.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','styles.css'],
+  theme: {
+    extend: {
+        fontFamily: {
+            'nunito': ['Nunito', 'sans-serif'],
+            'rubik': ['Rubik', 'sans-serif'],
+            'poppins': ['Poppins', 'sans-serif'],
+            'meri': ['Merriweather', 'serif'],
+        },
+    },
+  },
+  plugins: [],
+}
